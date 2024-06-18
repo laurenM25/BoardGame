e6da848c07829867a048b8d36dea4dd106e9c852
laurenM25 <lauren.marie.d25@gmail.com>
2024-06-17 20:43:08 -0400

    generated method for winning statistics each game

--- src/BoardGame.java
+++ src/BoardGame.java
@@ -70,7 +70,7 @@ public class BoardGame {
         players = new Player[n];
         for (int i = 0; i < n; i++) {
             players[i] = new Player(); // create player thing
-            switch (i) {
+            switch (i + 1) {
                 case 1:
                     players[i].player = 'A';
                     break;
@@ -92,31 +92,57 @@ public class BoardGame {
             System.out.print(spaceArray[i]);
     }
 
-    public void printBoard() { // 9, 7, 9 (squares) (+1 for end index)
-        char[] space = { '|', 'P', ' ', '|' };
-        char[] start = { '|', 'S', 'T', 'A', 'R', 'T', ' ', 'P', ' ', '|' };
-        char[] end = { '|', 'E', 'N', 'D', ' ', 'P', ' ', '|' };
+    public char[] resetCharArray(char[] space) { // so we don't have to create new variables
+        for (int i = 0; i < 4; i++) {
+            if (i == 0 || i == 3) {
+                space[i] = '|';
+            } else {
+                space[i] = ' ';
+            }
+        }
+        return space;
+    }
 
+    public void printBoard() { // 9, 7, 9 (squares) (+1 for end index)
+        char[] space = { '|', ' ', ' ', '|' };
+        char[] start = { '|', 'S', 'T', 'A', 'R', 'T', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
+        char[] end = { '|', 'E', 'N', 'D', ' ', ' ', ' ', '|' };
+
+        // see who is at start, player position 0
+        int startArrayIndex = 8;
+        for (int i = 0; i < players.length; i++) {
+            if (players[i].position == 0) {
+                start[7] = 'P';
+                start[startArrayIndex++] = players[i].player;
+            }
+        }
         printSpace(start);
 
+        // board spaces
         for (int i = 1; i <= board.size(); i++) {
             BoardSpace bs = board.moveNode(i);
             // check if occupied, print player if so
             if (bs.occupied) {
                 char player = bs.player;
+                space[1] = 'P';
                 space[2] = player;
+                // if end index...
+                if (i == 26) {
+                    end[5] = 'P';
+                    end[6] = player;
+                    break;
+                }
             }
             printSpace(space);
+            resetCharArray(space);
 
-            if (i == 9) {
-                System.out.println();
-            } // next line
-            if (i == 16) {
-                System.out.println();
-            } // next line
+            if (i == 9 || i == 16) {
+                System.out.print("\n             ");
+            }
 
         }
 
+        // end space
         printSpace(end);
         System.out.println();
     }
@@ -185,17 +211,26 @@ public class BoardGame {
         return rand.nextInt(6) + 1; // 1 to 6
     }
 
-    public boolean isGameOver() {
-        if (board.last().occupied) { // BUT CHECK POINTS!!! TO DO
-            return true;
+    public boolean isGameOver() { // assuming we just occupy the space
+        if (board.last().occupied) {
+            char play = board.last().player;
+            Player endPlayer = getPlayer(play);
+            if (endPlayer.points >= 44)
+                return true;
+            else
+                bootPlayerToStart(play);
         }
         return false;
     }
 
-    public void winner() {
+    public Player winner() {
+        if (!isGameOver()) {
+            return null;
+        }
         char pl = board.last().getPreviousPlayer();
         Player player = getPlayer(pl);
-        System.out.println("Player " + pl + " won the game with " + player.points + " points!");
+
+        return player;
     }
 
     public void makeMove() {
@@ -204,64 +239,199 @@ public class BoardGame {
         Player curPlayer = getPlayer(player);
         int currentSpace = getPlayerPosition(player); // find player's position
 
+        // move number DELETE PRINT MESSAGE
+        // System.out.println("MOVE " + curPlayer.movesMade);
+
         // roll die, examine space
         int move = rollDice();
-        System.out.println("roll: " + move);
+        // System.out.println("roll: " + move); //DELETE PRINT MESSAGE
 
         int bsIndex = move + currentSpace;
 
         // check if valid move/space
         if (bsIndex <= board.size()) { // a valid space
             BoardSpace bs = board.moveNode(bsIndex);
-            System.out.println("Move to position " + bsIndex);
+            // System.out.println("Move to position " + bsIndex); //DELETE PRINT MESSAGE
 
             // CHECK IF OCCUPIED
             if (bs.occupied) {
                 char previousPlayer = bs.getPreviousPlayer(); // get previous player occupying space
 
-                System.out.println(
-                        "Player " + previousPlayer + " previously occupied the space. They get booted to start!");
+                // System.out.println( //DELETE PRINT MESSAGE
+                // "Player " + previousPlayer + " previously occupied the space. They get booted
+                // to start!");
                 bootPlayerToStart(previousPlayer);
             }
-            // update board space (player new spot)
-            bs.playerTakesSpace(player);
+
+            boolean gameOver = false;
+            boolean continueSpace = true;
+            if (bsIndex == 26) { //
+                bs.playerTakesSpace(player); // take the space
+                gameOver = isGameOver();
+
+                if (!gameOver) { // haven't achieved min points to win, gets booted
+                    continueSpace = false;
+                    bs.playerLeavesSpace(); // bad, leave the space
+                }
+            }
+
+            if (continueSpace) {
+                // update board space (player new spot)
+                bs.playerTakesSpace(player);
+
+                // update player's points + info DELETE PRINT MESSAGES
+                // System.out.println("Player " + player + " gets " + bs.value + " points.");
+                curPlayer.points += bs.value;
+
+                // System.out.println("Player " + player + " now has " + curPlayer.points + "
+                // points.");
+                curPlayer.position = bsIndex;
+            }
             // make previous board space unoccupied
             if (currentSpace != 0) {
                 BoardSpace previousBS = board.moveNode(currentSpace);
                 previousBS.playerLeavesSpace();
             }
 
-            // update player's points + info
-            System.out.println("Player " + player + " gets " + bs.value + " points.");
-            curPlayer.points += bs.value;
-
-            System.out.println("Player " + player + " now has " + curPlayer.points + " points.");
-            curPlayer.position = bsIndex; // this could be a wrong line, double check index stuff
-
-        } else {
-            System.out.println("Overshot it! Player " + player + " loses a turn.");
+        } else { // DELETE PRINT MESSAGES
+            // System.out.println("Overshot it! Player " + player + " loses a turn.");
+            // System.out.println("current position: " + currentSpace + " | Points: " +
+            // curPlayer.points);
         }
         curPlayer.movesMade++;
         turn++; // next turn!
+
+        // System.out.println(); DELETE PRINT MESSAGE
+    }
+
+    public static void runOneGame(BoardGame game, int numPlayers, int gameNum) {
+        game.createBoard();
+
+        game.setPlayers(numPlayers);
+
+        do { // DELETE PRINT MESSAGE
+             // System.out.println("Player " + game.getWhoseTurn() + "'s
+             // turn:\n=====================================");
+            game.makeMove();
+        } while (!game.isGameOver());
+
+    }
+
+    public static void findAvgMovesToWin(int numPlayers) {
+        int sumA = 0;
+        int gamesWonA = 0;
+        int sumB = 0;
+        int gamesWonB = 0;
+        int sumC = 0;
+        int gamesWonC = 0;
+        int sumD = 0;
+        int gamesWonD = 0;
+        char letterW = ' ';
+        char[] players = new char[numPlayers];
+        String plysStr = "";
+
+        for (int i = 1; i <= 1000; i++) {
+            BoardGame game = new BoardGame();
+            runOneGame(game, numPlayers, i);
+            int movesToWin = game.winner().movesMade; // moves for winner to win
+
+            letterW = game.winner().player; // the winner type (ABCD)
+
+            switch (letterW) {
+                case 'A':
+                    sumA += movesToWin;
+                    gamesWonA++;
+                    break;
+                case 'B':
+                    sumB += movesToWin;
+                    gamesWonB++;
+                    break;
+                case 'C':
+                    sumC += movesToWin;
+                    gamesWonC++;
+                    break;
+                default:
+                    sumD += movesToWin;
+                    gamesWonD++;
+                    break;
+            }
+
+            if (i % 100 == 1) {
+                System.out.println("Game " + i);
+                game.printBoard();
+                System.out.println();
+            }
+
+            // list of people, collect info just once
+            if (i == 1) {
+                for (int p = 0; p < numPlayers; p++) {
+                    players[p] = game.players[p].player;
+                    plysStr += players[p];
+                    if (p < game.players.length - 1)
+                        plysStr += ", ";
+                }
+            }
+
+        }
+
+        int avgA = 0;
+        int avgB = 0;
+        int avgC = 0;
+        int avgD = 0;
+        // calculate any averages
+        if (gamesWonA > 0) {
+            avgA = sumA / gamesWonA; // careful of 0 denominator
+        }
+        if (gamesWonB > 0) {
+            avgB = sumB / gamesWonB;
+        }
+        if (gamesWonC > 0) {
+            avgC = sumC / gamesWonC;
+        }
+        if (gamesWonD > 0) {
+            avgD = sumD / gamesWonD;
+        }
+
+        // Results
+        System.out.println("RESULTS");
+        System.out.println("Players in game: " + plysStr);
+        for (int p = 1; p <= players.length; p++) {
+            int moves = 0;
+            int gamesWon = 0;
+            switch (p) {
+                case 1:
+                    moves = avgA;
+                    gamesWon = gamesWonA;
+                    break;
+                case 2:
+                    moves = avgB;
+                    gamesWon = gamesWonB;
+                    break;
+                case 3:
+                    moves = avgC;
+                    gamesWon = gamesWonC;
+                    break;
+                case 4:
+                    moves = avgD;
+                    gamesWon = gamesWonD;
+                    break;
+            }
+            String percentage = String.format("%.2f", (gamesWon / 1000.0) * 100);
+            System.out
+                    .println("Player " + players[p - 1] + " moves / avg % winning:   " + moves + " /  "
+                            + percentage
+                            + "%");
+        }
 
         System.out.println();
     }
 
     public static void main(String[] args) {
-        BoardGame game = new BoardGame();
-        game.createBoard();
 
-        game.printBoard();
-
-        game.setPlayers(3);
-
-        do {
-            System.out.println("Player " + game.getWhoseTurn() + "'s turn:\n=====================================");
-            game.makeMove();
-        } while (!game.isGameOver());
-
-        // final game message
-        game.winner();
+        findAvgMovesToWin(1);
+        findAvgMovesToWin(2);
+        findAvgMovesToWin(3);
+        findAvgMovesToWin(4);
     }
 
 }
